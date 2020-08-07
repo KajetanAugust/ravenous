4b761e75049e11e6cebf7bbee754999e05141b7b
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-08-07 16:20:31 +0200

    Created business component in Business.js, created styling for business component in Business.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+import Business from "../Business/Business";
 
 function App() {
   return (
