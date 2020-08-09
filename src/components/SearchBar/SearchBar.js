3a176392a47ec3a8a9d1f1cd6b6a4bb0ebe30bcc
KajetanAugust <56959772+KajetanAugust@users.noreply.github.com>
2020-08-09 19:42:58 +0200

    Added comments to SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,11 +17,13 @@ class SearchBar extends React.Component {
             "Highest Rated": 'rating',
             "Most Reviewed": 'review_count',
         }
+
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleLocationChange = this.handleLocationChange.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
     }
 
+    // checking for active sorting option
     getSortByClass(sortByOption) {
         if(this.state.sortBy === sortByOption) {
             return 'active'
@@ -30,30 +32,35 @@ class SearchBar extends React.Component {
         }
     }
 
+    // handling changes of sort options
     handleSortByChange(sortByOption) {
         this.setState({
             sortBy: sortByOption
         })
     }
 
+    // handling change of search term
     handleTermChange(e) {
         this.setState({
             term: e.target.value,
         })
     }
 
+    //handling change of location change
     handleLocationChange(e) {
         this.setState({
             location: e.target.value,
         })
     }
 
+    // handing search, passing input to searchYelp function in App.js
     handleSearch(e) {
         this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
 
         e.preventDefault();
     }
 
+    // rendering sorting options by iterating through this.state.sortByOptions and creating <li> elements for each of them
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -61,6 +68,7 @@ class SearchBar extends React.Component {
         })
     }
 
+    // rendering search bar component content
     render() {
         return(
             <div className="SearchBar">
